91b39ef1b25dbdc9c85e477b41ca4ed5bd5b97a6
Yuuna Kurita <yuuna.m@gmail.com>
2014-03-25 13:02:10 +0900

    一度修正

--- lib/fluent/plugin/in_mqtt.rb
+++ lib/fluent/plugin/in_mqtt.rb
@@ -10,7 +10,8 @@ module Fluent
     
     config_param :port, :integer, :default => 1883
     config_param :bind, :string, :default => '127.0.0.1'
-    config_param :topic, :string, :default => '#'
+    config_param :topic, :string, :default => 'api/#'
+    config_param :match_topic, :string, :default => 'api'
 
     require 'mqtt'
 
@@ -19,15 +20,17 @@ module Fluent
       @bind ||= conf['bind']
       @topic ||= conf['topic']
       @port ||= conf['port']
+      @match_topic ||= conf['match_topic']
     end
 
     def start
-      $log.debug "start mqtt"
+      $log.debug "start mqtt #{@bind}"
       @connect = MQTT::Client.connect({remote_host: @bind, remote_port: @port})
       @connect.subscribe(@topic)
 
       @thread = Thread.new do
         @connect.get do |topic,message|
+          next if topic.scan(/#{@match_topic}/).length == 0
           topic.gsub!("/","\.")
           $log.debug "#{topic}: #{message}"
           emit topic, json_parse(message)
@@ -36,7 +39,14 @@ module Fluent
     end
 
     def emit topic, message , time = Fluent::Engine.now
-          Fluent::Engine.emit(topic, time , message )
+      if message.class == Array
+        message.each do |data|
+          $log.debug "#{topic}: #{data}"
+          Fluent::Engine.emit(topic , time , data)
+        end
+      else
+        Fluent::Engine.emit(topic , time , message)
+      end
     end
 
     def json_parse message
