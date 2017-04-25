0807dc51d0142112c207865bb5f6c3b3fc8e39c9
Hiroshi Hatake <hatake@clear-code.com>
2017-04-25 13:42:38 +0900

    Add json formatting test case

--- test/plugin/test_out_mqtt.rb
+++ test/plugin/test_out_mqtt.rb
@@ -80,5 +80,39 @@ class MqttOutputTest < Test::Unit::TestCase
         assert_equal "\"2011-01-02T22:14:15+0900\",\"#{data[i][:message]}\"\n", record[1]
       end
     end
+
+    def test_format_json
+      ENV['TZ'] = 'Asia/Tokyo'
+
+      d = create_driver(
+        %[ bind 127.0.0.1
+           port 1883
+           format json
+           time_type string
+           time_format %Y-%m-%dT%H:%M:%S%z
+           fields time,message]
+      )
+
+      client = sub_client
+      time = event_time("2011-01-02 13:14:15 UTC")
+      data = [
+        {tag: "tag1", message: "#{time},hello world" },
+        {tag: "tag2", message: "#{time},hello to you to" },
+        {tag: "tag3", message: "#{time}," },
+      ]
+
+      d.run(default_tag: "test") do
+        data.each do |record|
+          d.feed(time, record)
+        end
+      end
+      3.times do |i|
+        record = client.get
+        assert_equal "td-agent", record[0]
+        assert_equal "{\"tag\":\"#{data[i][:tag]}\",\
+\"message\":\"#{data[i][:message]}\",\
+\"time\":\"2011-01-02T22:14:15+0900\"}\n", record[1]
+      end
+    end
   end
 end
